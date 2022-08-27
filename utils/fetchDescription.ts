f2881fa713eb046e82f37e1acbfa52b33b977849
breezeH264 <vvderrick@gmail.com>
2022-08-27 16:11:55 -0400

    feat: improve responsiveness on mobile devices

--- utils/fetchDescription.ts
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://pokeapi.co/api/v2';
-
-const query = {
-  species: 'pokemon-species',
-};
-
-export async function fetchDescription(species: number) {
-  return fetch(`${BASE_URL}/${query.species}/${species}`);
-}
